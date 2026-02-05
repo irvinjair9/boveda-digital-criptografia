f00637f2ae9c85d188d0d93050aad37cb5066ba0
Irvin Carbajal <irvin.carbajal@pepsico.com>
2026-02-05 15:47:01 -0600

    Creación del backend

---
+++ boveda-digital-api/src/test/java/com/example/boveda_digital_api/BovedaDigitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boveda_digital_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BovedaDigitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
