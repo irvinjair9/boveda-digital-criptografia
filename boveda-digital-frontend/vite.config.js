6d85d4c7d3dfb0d41c9ab60d3d102bdafa6e34e1
Irvin Carbajal <irvin.carbajal@pepsico.com>
2026-02-05 16:40:03 -0600

    cambio de puerto del frontend

--- boveda-digital-frontend/vite.config.js
+++ boveda-digital-frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+      
+    port: 5000,        
+      strictPort: true,  
+      host: true,        
+      open: true,        
+      proxy: {
+        '/api': 'http://localhost:8080' // backend de Spring Boot
+      }
+
+    }
 })
+
+
